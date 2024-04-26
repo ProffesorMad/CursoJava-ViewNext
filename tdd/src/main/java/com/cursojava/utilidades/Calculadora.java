d96c7d8a6c7eda0731c28826df7c27cb84b16c03
Pablo Gracia Gálvez <6003056@viewnext.com>
2024-04-26 10:37:26 +0200

    Test

---
+++ tdd/src/main/java/com/cursojava/utilidades/Calculadora.java
@@ -0,0 +1,9 @@
+package com.cursojava.utilidades;
+
+public class Calculadora {
+	
+	public double sumar(double numero1, double numero2) {
+		return numero1+numero2;
+	}
+
+}
---
+++ tdd/src/test/java/com/cursojava/test/utilidades/CalculadoraTest.java
@@ -0,0 +1,20 @@
+package com.cursojava.test.utilidades;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import com.cursojava.utilidades.Calculadora;
+
+class CalculadoraTest {
+
+	@Test
+	void testSumar() {
+		Calculadora calculadora = new Calculadora();
+		
+		double suma = calculadora.sumar(4, 3);
+		
+		assertEquals(7,suma);
+	}
+
+}
