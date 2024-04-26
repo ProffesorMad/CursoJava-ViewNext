dedf5d2caed38d7da4953d89f9ede2053c79b8a7
Pablo Gracia Gálvez <6003056@viewnext.com>
2024-04-26 11:52:44 +0200

    PesetasTest

--- tdd/src/test/java/com/cursojava/test/utilidades/PesetasTest.java
+++ tdd/src/test/java/com/cursojava/test/utilidades/PesetasTest.java
@@ -10,11 +10,11 @@ public class PesetasTest {
 	
     @Test
     public void testEurosAPesetas() {
-        assertEquals(1663.86, Pesetas.eurosAPesetas(10), 0.01); // Verifica que 10 euros sean 1663.86 pesetas
+        assertEquals(1663.86, Pesetas.eurosAPesetas(10), 0.01); // Verifica que 10 euros sean 1663.86 pesetas.
     }
 
     @Test
     public void testPesetasAEuros() {
-        assertEquals(601.01, Pesetas.pesetasAEuros(100000), 0.01); // Verifica que 10000 pesetas sean 60 euros
+        assertEquals(601.01, Pesetas.pesetasAEuros(100000), 0.01); // Verifica que 10000 pesetas sean 60 euros.
     }
 }
