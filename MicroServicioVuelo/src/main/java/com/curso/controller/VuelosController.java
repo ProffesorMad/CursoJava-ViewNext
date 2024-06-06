4a503672fbd588332f3d1bb43828deae49764feb
Pablo Gracia Gálvez <6003056@viewnext.com>
2024-06-07 01:01:20 +0200

    MicroServicioVuelo Terminado

--- MicroServicioVuelo/src/main/java/com/curso/controller/VuelosController.java
+++ MicroServicioVuelo/src/main/java/com/curso/controller/VuelosController.java
@@ -23,43 +23,43 @@ public class VuelosController {
 	@Autowired
 	VuelosService service;
 
-	// http://localhost:8080/vuelos
+	// http://localhost:7000/vuelos
 	@GetMapping(value = "vuelos", produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<Vuelo> listarVuelos() {
 	    return service.listar();
 	}
 
-	// http://localhost:8080/vuelos/8
+	// http://localhost:7000/vuelos/8
 	@GetMapping(value = "vuelos/{idVuelo}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public Optional<Vuelo> buscarVuelo(@PathVariable("idVuelo") int idVuelo) {
 	    return service.buscar(idVuelo);
 	}
 
-	// http://localhost:8080/vuelos {"empresa": "AirlineX","fechaVuelo": "2024-07-01","precio": 350.0,"plazasDisponibles": 150}
+	// http://localhost:7000/vuelos {"empresa": "AirlineX","fechaVuelo": "2024-07-01","precio": 350.0,"plazasDisponibles": 150}
 	@PostMapping(value = "vuelos", consumes = MediaType.APPLICATION_JSON_VALUE)
 	public void crearVuelo(@RequestBody Vuelo vuelo) {
 	    service.crear(vuelo);
 	}
 
-	// http://localhost:8080/vuelos {"idVuelo": 1,"empresa": "AirlineY","fechaVuelo": "2024-07-05","precio": 400.0,"plazasDisponibles": 120}
+	// http://localhost:7000/vuelos {"idVuelo": 1,"empresa": "AirlineY","fechaVuelo": "2024-07-05","precio": 400.0,"plazasDisponibles": 120}
 	@PutMapping(value = "vuelos", consumes = MediaType.APPLICATION_JSON_VALUE)
 	public void actualizarVuelo(@RequestBody Vuelo vuelo) {
 	    service.actualizar(vuelo);
 	}
 
-	// http://localhost:8080/vuelos/2
+	// http://localhost:7000/vuelos/2
 	@DeleteMapping(value = "vuelos/{idVuelo}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<Vuelo> eliminarVuelo(@PathVariable("idVuelo") int idVuelo) {
 	    return service.eliminar(idVuelo);
 	}
 
-	// http://localhost:8080/vuelos/disponibles/100
+	// http://localhost:7000/vuelos/disponibles/100
 	@GetMapping(value = "vuelos/disponibles/{plazas}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<Vuelo> buscarVuelosDisponibles(@PathVariable("plazas") int plazas) {
 	    return service.buscarDisponibles(plazas);
 	}
 
-	// http://localhost:8080/vuelos/1/120
+	// http://localhost:7000/vuelos/1/120
 	@PutMapping(value = "vuelos/{idVuelo}/{plazas}")
 	public void actualizarPlazasVuelo(@PathVariable("idVuelo") int idVuelo, @PathVariable("plazas") int plazas) {
 	    service.actualizarPlazas(plazas, idVuelo);
